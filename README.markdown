0cce657b4385eaef4e403578549e6dabcb25dd19
Rufus Pollock <rufus.pollock@okfn.org>
2012-04-30 20:52:09 +0100

    [all][m]: working prototype that reads a gist, runs it and displays it.

---
+++ README.markdown
@@ -0,0 +1,14 @@
+Run (and write) gists from the browser.
+
+## Install
+
+Initialize submodules:
+
+    git submodule update --init
+
+Open the page:
+
+    chrome index.html
+
+That's it!
+
---
+++ app.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function() {
+  var qs = parseQueryString(window.location.search);
+  if (qs) {
+    loadGist(qs.gist_id || qs.gist_url);
+  }
+
+  $('.js-gist-info').submit(function(e) {
+    e.preventDefault();
+    loadGist($('.gist-url').val());
+  });
+})
+
+function loadGist(gistIdOrUrl) {
+  var gistId = gistIdOrUrl;
+  if (gistIdOrUrl.indexOf('http') != -1) {
+    var parts = normalUrl.split('/');
+    gistId = parts[parts.length-1];
+  }
+  var url = 'https://api.github.com/gists/' + gistId;
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(data) {
+      $.each(data.data.files, function(key,value) {
+        $('.js-data').val(value.content);
+        eval(value.content);
+        return false;
+      });
+    }
+  });
+}
+
+// Parse a URL query string (?xyz=abc...) into a dictionary.
+parseQueryString = function(q) {
+  if (!q) {
+    return {};
+  }
+  var urlParams = {},
+    e, d = function (s) {
+      return unescape(s.replace(/\+/g, " "));
+    },
+    r = /([^&=]+)=?([^&]*)/g;
+
+  if (q && q.length && q[0] === '?') {
+    q = q.slice(1);
+  }
+  while (e = r.exec(q)) {
+    // TODO: have values be array as query string allow repetition of keys
+    urlParams[d(e[1])] = d(e[2]);
+  }
+  return urlParams;
+};
