89cf5da1ce1e70351a43a2717f3066114f6a65f2
Duke <duke@riseup.net>
2012-05-20 15:50:57 -0300

    moved github code to external code

---
+++ github.js
@@ -0,0 +1,21 @@
+var github = function(){
+  var config = {
+    "localhost": {
+      clientId: "35263dbf225ca2e14de3",
+      clientSecret: "e8761dc7e889893764b1af6f2a3d52f6a6541f58",
+      authServer: "http://getmygist.herokuapp.com/token"
+    },
+    "okfn-br.github.com": {
+      clientId: "0fc40d9e0188c351a069",
+      clientSecret: "344cef773df669a8fbb5d0ab086d245fab161ba8",
+      authServer: "http://getmygist.herokuapp.com/token"
+    }
+  }[window.location.host];
+
+  return $.extend(config, {
+    scope: "user,gist",
+  });
+};
+
+
+
